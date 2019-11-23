764ce3ff96c909185c6f63965560da0b7acd63b8
SriSreedhar <srisreedhar@live.com>
2019-11-24 00:20:14 +0530

    added onclick func,linked wit form-props

--- src/App.js
+++ src/App.js
@@ -19,24 +19,34 @@ class HomePage extends Component {
     error:"API Error - check Inputs provided"
   }
 
-  getData = async(event)=>{
+  getDatafromAPI = async(event)=>{
     event.preventDefault();
     const city= event.target.elements.cityForm.value;
-    const country=event.target.elements.countryForm.value;
+    const country=event.target.elements.formCountry.value;
     // secure API key before pushing
     const apiUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city},${country}&mode=json&units=metric&APPID=2d80cf7142a085e6c34f383205d35118`;
     const apiresponse= await fetch(apiUrl);
     const jsondata= await apiresponse.json();
     console.log(jsondata);
-
+    this.setState({
+      temparature:jsondata.list[0].temp.day,
+      maxTemp:jsondata.list[0].temp.max,
+      minTemp:jsondata.list[0].temp.min,
+      description:jsondata.list[0].weather[0].description,
+      city:jsondata.city.name,
+      mapLon:jsondata.city.coord.lon,
+      mapLat:jsondata.city.coord.lat,
+      country:jsondata.city.country,
+      error:"API Error - check Inputs provided"
+    })
   }
 
-  
+
   render() { 
     return (
       <Fragment>
             <Navbar/>
-            <UserInput />
+            <UserInput getData={this.getDatafromAPI} />
             <WeatherToday/>
       </Fragment>
             );
