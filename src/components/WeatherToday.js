215fb6a115e72a112a1476dc536ceb3148f0347d
SriSreedhar <srisreedhar@live.com>
2019-11-23 22:28:27 +0530

    created component for today's wethr

---
+++ src/components/WeatherToday.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+// gets current day's weathe from the api
+class weathertoday extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default weathertoday;
